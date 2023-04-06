25dbf188e66158b48bdb00f25cc0a90a0194639d
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:37:49 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+int main() {
+    std::cout << "2000\n";
+    for(int i=0; i<1000;++i){std::cout << "1 ";}
+    for(int i=0; i<1000;++i){std::cout << "1000 ";}
+    return 0;
+}
