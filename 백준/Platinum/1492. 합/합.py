e02963f3e14ab484ef6bc3acec7430418b96a218
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:02:49 +0900

    [Platinum II] Title: 합, Time: 128 ms, Memory: 124504 KB -BaekjoonHub

---
+++ 백준/Platinum/1492. 합/합.py
@@ -0,0 +1,21 @@
+import sys
+MOD = 1000000007
+N, K = map(int, sys.stdin.readline().split())
+
+f = [1] * 51
+fac = [1] * 52
+f[0] = N % MOD
+f[1] = ((N * (N+1))//2) % MOD
+
+for i in range(1,52):
+    fac[i] = i * fac[i-1]
+
+
+for k in range(2,K+1):
+    ans = pow(N+1,k+1,MOD) - 1
+    for p in range(0,k):
+        ans = (ans  - ((fac[k+1]//fac[p])//fac[k+1-p])*f[p]) % MOD
+    ans = (ans * pow(k+1,MOD-2,MOD)) % MOD
+    f[k] = ans
+
+print(f[K])
