3fc690bb26cc062c73e7684b4b5deb05b41470d3
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:17:36 +0900

    [Gold I] Title: 자리합, Time: 132 ms, Memory: 125336 KB -BaekjoonHub

---
+++ 백준/Gold/5425. 자리합/자리합.py
@@ -0,0 +1,20 @@
+import sys
+
+def calc_sum(num):
+    if num==-1: return 0
+    count = 0
+    for idx, char in enumerate(str(num)):
+        if idx!=0:
+            count += int(str(num)[:idx]) * 45 * (10**(len(str(num))-1-idx))
+        count += sum(range(int(char))) * (10**(len(str(num))-1-idx))
+        if idx!=len(str(num))-1:
+            count += int(char) * (int(str(num)[idx+1:]) + 1)
+        else:
+            count += int(char)
+    return count
+
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    A, B = map(int,sys.stdin.readline().split())
+    print(calc_sum(B)-calc_sum(A-1))
