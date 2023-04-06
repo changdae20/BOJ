74349b7fb5056a92a79a64d5ffc4923d5592429a
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:13:24 +0900

    [Silver IV] Title: 암기왕, Time: 704 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[]){
+#ifndef BOJ
+    freopen(argv[1], "r", stdin);
+#endif
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio( false );
+    std::cout.tie(NULL);
+    int T;
+    std::cin >> T;
+    while(T--){
+        int N, x;
+        std::cin >> N;
+        std::vector<int> v(N, 0);
+        for(auto &el : v) std::cin >> el;
+
+        std::sort(v.begin(), v.end());
+
+        std::cin >> N;
+
+        while(N--){
+            std::cin >> x;
+            std::cout << static_cast<bool>(std::distance(std::lower_bound(v.begin(), v.end(), x), std::upper_bound(v.begin(), v.end(), x))) << '\n';
+        }
+    }
+
+    
+    return 0;
+}
