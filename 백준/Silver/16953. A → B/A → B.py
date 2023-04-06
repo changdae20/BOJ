68ee1fe539d7faa4df55df2453f20c2fe125490b
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:43:48 +0900

    [Silver II] Title: A → B, Time: 72 ms, Memory: 31268 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,17 @@
+import heapq
+A,B = map(int,input().split())
+
+count = 0
+while(B>A):
+    if(B%2==0):
+        B /= 2
+        count +=1
+    elif(B%10==1):
+        B = B//10
+        count +=1
+    else: break
+    
+if(B==A):print(count+1)
+else: print("-1")
+    
+
