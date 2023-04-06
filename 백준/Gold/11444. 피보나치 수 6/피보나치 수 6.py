206ad89b16a554a6ede7e415304b10c2ea227b96
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:28:32 +0900

    [Gold II] Title: 피보나치 수 6, Time: 76 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,20 @@
+def mult(a,b,m):
+    return [[(a[0][0]*b[0][0]+a[0][1]*b[1][0])%m,(a[0][0]*b[0][1]+a[0][1]*b[1][1])%m],[(a[1][0]*b[0][0]+a[1][1]*b[1][0])%m,(a[1][0]*b[0][1]+a[1][1]*b[1][1])%m]]
+
+a = [[1,1],[1,0]]
+tmp = [[1,1],[1,0]]
+n = int(input())
+bin_n = bin(n)[2:][::-1]
+for i in range(len(bin_n)):
+    if i==0 and bin_n[i]=='1':
+        tmp = mult(tmp,tmp,1000000007)
+    elif i==0 and bin_n[i]=='0':
+        a = [[1,0],[0,1]]
+        tmp = mult(tmp,tmp,1000000007)
+    elif bin_n[i]=='0':
+        tmp = mult(tmp,tmp,1000000007)
+    else:
+        a = mult(a,tmp,1000000007)
+        tmp = mult(tmp,tmp,1000000007)
+
+print(a[0][1])
