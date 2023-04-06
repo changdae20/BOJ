7c8a6553fe18f822f4fc1664dc2941c15a6b6f77
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:48:45 +0900

    [Silver III] Title: 선린의 터를, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/21968. 선린의 터를/선린의 터를.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <type_traits>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+std::vector<int> bin(uint64_t num){
+    std::vector<int> v;
+    do{
+        v.push_back(num%2);
+    }while(num/=2);
+    return v;
+}
+
+uint64_t new_pow(uint64_t a, const int& b){
+    auto v = bin(b);
+    uint64_t ans = 1;
+    std::for_each(v.begin(), v.end(), [&ans, &a](auto& el){
+        if(el) ans*=a;
+        a*=a;
+    });
+    return ans;
+}
+
+int main(){
+    int T;
+    std::cin >> T;
+    while(T--){
+        uint64_t num;
+        std::cin >> num;
+        auto v = bin(num);
+        std::cout << std::accumulate(v.begin(), v.end(), 0LL, [&v](const auto &a, const auto &b){ return a + ( b * new_pow(3, &b - &v[0]) ); }) << '\n';
+    }
+    return 0;
+}
