dc68e85f15f7182a212b75af6c96ce1aabc5e0f2
Changdae Son <46400212+changdae20@users.noreply.github.com>
2023-04-07 00:01:49 +0900

    [Gold I] Title: 커피숍2, Time: 1540 ms, Memory: 156040 KB -BaekjoonHub

---
+++ 백준/Gold/1275. 커피숍2/커피숍2.py
@@ -0,0 +1,39 @@
+import sys
+N, Q= map(int,sys.stdin.readline().split())
+
+tree = [0] * 4*N
+
+def get_value(cur_node, start_of_this_node, end_of_this_node, index):
+    if start_of_this_node==end_of_this_node:
+        return tree[cur_node]
+    mid = (start_of_this_node + end_of_this_node) // 2
+    if index <= mid : return get_value(2*cur_node, start_of_this_node, mid, index)
+    else: return get_value(2*cur_node + 1, mid+1, end_of_this_node , index)
+
+def update(cur_node, start_of_this_node, end_of_this_node, update_idx, update_value):
+    tree[cur_node] += update_value
+    if start_of_this_node == end_of_this_node: return
+    mid = (start_of_this_node + end_of_this_node) // 2
+    if update_idx <= mid : update(cur_node*2, start_of_this_node, mid, update_idx, update_value)
+    else: update(cur_node*2+1, mid+1, end_of_this_node, update_idx, update_value)
+
+def get_sum(cur_node, start_of_this_node, end_of_this_node, start_of_sum, end_of_sum):
+    if start_of_this_node==start_of_sum and end_of_this_node==end_of_sum : return tree[cur_node]
+    mid = (start_of_this_node + end_of_this_node) // 2
+    if end_of_sum <= mid:
+        return get_sum(2*cur_node, start_of_this_node, mid, start_of_sum, end_of_sum)
+    elif start_of_sum > mid:
+        return get_sum(2*cur_node + 1, mid + 1, end_of_this_node, start_of_sum, end_of_sum)
+    else:
+        return get_sum(2*cur_node, start_of_this_node, mid, start_of_sum, mid) + get_sum(2*cur_node + 1, mid + 1, end_of_this_node, mid+1, end_of_sum)
+
+init = list(map(int,sys.stdin.readline().split()))
+for i in range(1,N+1):
+    update(1,1,N,i,init[i-1])
+
+for _ in range(Q):
+    first, second, third, fourth = map(int,sys.stdin.readline().split())
+    print(get_sum(1,1,N,min(first,second),max(first,second)))
+    orig = get_value(1,1,N,third)
+    if orig!=fourth: update(1,1,N,third, fourth-orig)
+        
